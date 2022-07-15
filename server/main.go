726b5cd58e5761bdc7a0906d6ed9bde67e7675e9
Ali Elbashir <alielbashiir@gmail.com>
2022-07-16 01:10:15 +0300

    Rename current_state and add comments

--- server/main.go
+++ server/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/koustech/mastermind/state"
 	"io"
 	"log"
 	"net"
 	"sync"
 
-	// This import path is based on the name declaration in the go.mod,
-	// and the gen/proto/go output location in the buf.gen.yaml.
-	mm "github.com/koustech/mastermind"
 	pb "github.com/koustech/mastermind/gen/proto/go/mastermind/v1"
 	"google.golang.org/grpc"
 )
@@ -40,14 +38,15 @@ func run() error {
 // mastermindServiceServer implements the MastermindService API.
 type mastermindServiceServer struct {
 	pb.UnimplementedMastermindServiceServer
-	mu            sync.Mutex // protects current_state
-	current_state pb.MissionState
+	mu           sync.Mutex // protects currentState
+	currentState pb.MissionState
 }
 
 // UpdateState updates the current state according to the state transition table
 func (s *mastermindServiceServer) UpdateState(stream pb.MastermindService_UpdateStateServer) error {
-	s.current_state = pb.MissionState_MISSION_STATE_APPROACH
+	s.currentState = pb.MissionState_MISSION_STATE_APPROACH
 	for {
+		// durum guncellemesini al
 		req, err := stream.Recv()
 		if err == io.EOF {
 			return nil
@@ -56,15 +55,16 @@ func (s *mastermindServiceServer) UpdateState(stream pb.MastermindService_Update
 			return err
 		}
 		s.mu.Lock()
-		old_state := s.current_state
-		s.current_state, err = mm.ResolveState(req.StateTransition, s.current_state)
+		oldState := s.currentState
+		// yeni durumu hesapla ve yeni durumu guncelle
+		s.currentState, err = state.ResolveState(req.StateTransition, s.currentState)
 		s.mu.Unlock()
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
-		if err := stream.Send(&pb.UpdateStateResponse{OldState: old_state, StateTransition: req.StateTransition, CurrentState: s.current_state}); err != nil {
+		// yeni durumu istemciye gonder
+		if err := stream.Send(&pb.UpdateStateResponse{OldState: oldState, StateTransition: req.StateTransition, CurrentState: s.currentState}); err != nil {
 			return err
 		}
-
 	}
 }
