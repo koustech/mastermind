081c65bc005fac089085485b51bd7f36b8d72839
Ali Elbashir <alielbashiir@gmail.com>
2022-08-03 01:23:47 +0300

    Add test mode for testing without telemetry

--- main/main.go
+++ main/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/aler9/gomavlib"
+	"github.com/koustech/mastermind/telemetry"
 
 	"github.com/koustech/mastermind/server"
-	"github.com/koustech/mastermind/telemetry"
 	u "github.com/koustech/mastermind/utils"
 )
 
@@ -14,11 +15,21 @@ func main() {
 
 	var grpcAddress string
 	var mavlinkAddress string
+	var test bool
 	flag.StringVar(&grpcAddress, "a", "0.0.0.0:5678", "Mastermind's gRPC server address")
 	flag.StringVar(&mavlinkAddress, "m", "127.0.0.1:14556", "The source MAVLink vehicle's ip address")
+	flag.BoolVar(&test, "test", false, "Disables telemetry mode")
 	flag.Parse()
 
-	node := telemetry.ConnectToVehicle(mavlinkAddress)
+	var node *gomavlib.Node
+
+	if test {
+		u.Logger.Info("Running in test mode")
+		node = &gomavlib.Node{}
+	} else {
+		u.Logger.Info("Running in telemetry mode")
+		node = telemetry.ConnectToVehicle(mavlinkAddress)
+	}
 	defer node.Close()
 
 	if err := server.Run(grpcAddress, node); err != nil {
