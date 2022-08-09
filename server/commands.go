efd0d2b9f642e31db720822875969cfaa6efb1b5
Ali Elbashir <alielbashiir@gmail.com>
2022-08-09 23:32:56 +0300

    Add PIDlevel command

---
+++ server/commands.go
@@ -0,0 +1,174 @@
+package server
+
+import (
+	"context"
+	pb "github.com/koustech/mastermind/gen/go/proto/mastermind/v1"
+	"github.com/koustech/mastermind/utils"
+)
+
+// const (
+// 	EventNewTelemetry = evbus.EventID("new_telemetry")
+// )
+
+// type NewTelemetryEvent struct {
+// 	response *pb.GetDetailedTelemetryResponse
+// }
+
+// func (e *NewTelemetryEvent) EventID() evbus.EventID {
+// 	return EventNewTelemetry
+// }
+
+// func GetTelem(bus evbus.Bus, node *gomavlib.Node) {
+// 	vfrHud := ardupilotmega.MessageVfrHud{}
+// 	attitude := ardupilotmega.MessageAttitude{}
+// 	globalPositionInt := ardupilotmega.MessageGlobalPositionInt{}
+// 	navControllerOutput := ardupilotmega.MessageNavControllerOutput{}
+// 	batteryStatus := ardupilotmega.MessageBatteryStatus{}
+
+// 	var timeBootMs uint64
+
+// 	var updateFlag bool
+// 	// print every message we receive
+// 	for evt := range node.Events() {
+// 		updateFlag = true
+
+// 		if frm, ok := evt.(*gomavlib.EventFrame); ok {
+// 			switch msg := frm.Message().(type) {
+// 			case *ardupilotmega.MessageAttitude:
+// 				attitude = *msg
+// 				// timeBootMs = msg.TimeBootMs
+// 				// utils.Logger.Debug("timebootms updated by attitude: ", timeBootMs)
+// 			case *ardupilotmega.MessageVfrHud:
+// 				vfrHud = *msg
+// 			case *ardupilotmega.MessageGlobalPositionInt:
+// 				globalPositionInt = *msg
+// 				// timeBootMs = msg.TimeBootMs
+// 				// utils.Logger.Debug("timebootms updated by gpsint: ", timeBootMs)
+// 			case *ardupilotmega.MessageNavControllerOutput:
+// 				navControllerOutput = *msg
+// 			case *ardupilotmega.MessageBatteryStatus:
+// 				batteryStatus = *msg
+// 				updateFlag = false // Don't want to increase message sending frequency just for battery
+// 			case *ardupilotmega.MessageSystemTime:
+// 				utils.Logger.Debug("timeunixsec by systemTime: ", msg.TimeUnixUsec)
+// 				utils.Logger.Debug("timeunixsec / 1000 by systemTime: ", msg.TimeUnixUsec/1000)
+
+// 				timeBootMs = msg.TimeUnixUsec / 1000
+// 				utils.Logger.Debug("timebootms updated by systemTime: ", timeBootMs)
+// 			default:
+// 				updateFlag = false
+// 			}
+
+// 			if updateFlag {
+// 				bus.Publish(&NewTelemetryEvent{
+// 					response: &pb.GetDetailedTelemetryResponse{
+// 						TimeBootMs:  timeBootMs,
+// 						Lat:         globalPositionInt.Lat,
+// 						Lon:         globalPositionInt.Lon,
+// 						RelativeAlt: globalPositionInt.RelativeAlt,
+// 						Roll:        attitude.Roll,
+// 						Pitch:       attitude.Pitch,
+// 						Yaw:         attitude.Yaw,
+// 						Airspeed:    vfrHud.Airspeed,
+// 						Groundspeed: vfrHud.Groundspeed,
+// 						WpDist:      uint32(navControllerOutput.WpDist),
+// 						Battery:     int32(batteryStatus.BatteryRemaining),
+// 						Autonomous:  false, // FIXME: change from false to according to mode
+// 						Target:      nil,
+// 					},
+// 				})
+// 			}
+// 		}
+// 	}
+// }
+
+// // GetTelemetry sends a stream of telemetry to all clients
+// func (s *mastermindServiceServer) GetTelemetry(_ *pb.GetTelemetryRequest, stream pb.MastermindService_GetTelemetryServer) error {
+// 	// generate new sessionId and add channel
+// 	sessionId := uuid.New()
+// 	u.Logger.Info("new connection for session: ", sessionId)
+
+// 	errChan := make(chan error)
+
+// 	s.telemUpdateHandlers[sessionId] = s.stateBus.Subscribe(EventNewTelemetry, func(e evbus.Event) {
+// 		se := e.(*NewTelemetryEvent)
+// 		// change detailed telemetry to normal telem response
+// 		response := &pb.GetTelemetryResponse{
+// 			TimeBootMs:  se.response.TimeBootMs,
+// 			Lat:         se.response.Lat,
+// 			Lon:         se.response.Lon,
+// 			RelativeAlt: se.response.RelativeAlt,
+// 			Roll:        se.response.Roll,
+// 			Pitch:       se.response.Pitch,
+// 			Yaw:         se.response.Yaw,
+// 			Airspeed:    se.response.Airspeed,
+// 			Groundspeed: se.response.Groundspeed,
+// 			WpDist:      se.response.WpDist,
+// 		}
+// 		if err := stream.Send(response); err != nil {
+// 			s.stateBus.Unsubscribe(s.telemUpdateHandlers[sessionId])
+// 			delete(s.telemUpdateHandlers, sessionId)
+// 			u.Logger.Infof("unsubscribed telem session: %v from event bus", sessionId)
+
+// 			errChan <- err
+// 		}
+// 	})
+
+// 	<-errChan
+// 	return nil
+// }
+
+//// GetDetailedTelemetry sends a stream of detailed telemetry to all clients
+//func (s *mastermindServiceServer) GetDetailedTelemetry(_ *pb.GetDetailedTelemetryRequest, stream pb.MastermindService_GetDetailedTelemetryServer) error {
+//	// generate new sessionId and add channel
+//	sessionId := uuid.New()
+//	u.Logger.Info("new connection for session: ", sessionId)
+//
+//	errChan := make(chan error)
+//
+//	s.telemUpdateHandlers[sessionId] = s.stateBus.Subscribe(EventNewTelemetry, func(e evbus.Event) {
+//		se := e.(*NewTelemetryEvent)
+//		response := se.response
+//		if err := stream.Send(response); err != nil {
+//			s.stateBus.Unsubscribe(s.telemUpdateHandlers[sessionId])
+//			delete(s.telemUpdateHandlers, sessionId)
+//			u.Logger.Infof("unsubscribed telem session: %v from event bus", sessionId)
+//
+//			errChan <- err
+//		}
+//	})
+//
+//	<-errChan
+//	return nil
+//}
+
+// SetPIDLevel sets the PID of the plane according to predetermined levels in the config
+func (s *mastermindServiceServer) SetPIDLevel(_ context.Context, request *pb.SetPIDLevelRequest) (*pb.SetPIDLevelResponse, error) {
+	// FIXME: add actual command invocation
+	utils.Logger.Infof("PID Level %v request received", request.Level)
+	success := true
+
+	switch request.Level {
+	case pb.PIDLevel_PID_LEVEL_1:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_2:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_3:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_4:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_5:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_6:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_7:
+		utils.Logger.Info("PID set to ", request.Level)
+	case pb.PIDLevel_PID_LEVEL_8:
+		utils.Logger.Info("PID set to ", request.Level)
+	default:
+		utils.Logger.Errorf("level %v not supported", request.Level)
+		success = false
+	}
+
+	return &pb.SetPIDLevelResponse{CommandSucceeded: success}, nil
+}
