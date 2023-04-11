1ea7e4d23efeca2b19874618428d478cf22230a7
Ali Elbashir <alielbashiir@gmail.com>
2023-04-11 07:44:05 +0300

    Change setAttitude command to use new kstch mode

--- server/commands.go
+++ server/commands.go
@@ -9,6 +9,8 @@ import (
 	"github.com/koustech/mastermind/utils"
 )
 
+const MAV_CMD_NAV_LOCK_KSTCH = 151 // command ID of custom command to move plane
+
 // SetPIDLevel sets the PID of the plane according to predetermined levels in the config
 func (s *mastermindServiceServer) SetPIDLevel(_ context.Context, request *pb.SetPIDLevelRequest) (*pb.SetPIDLevelResponse, error) {
 	// FIXME: add actual command invocation
@@ -81,7 +83,7 @@ func (s *mastermindServiceServer) GotoWaypoint(_ context.Context, request *pb.Go
 		X:               request.Lat,
 		Y:               request.Lon,
 		Z:               request.Alt,
-		MissionType: ardupilotmega.MAV_MISSION_TYPE_MISSION,
+		MissionType:     ardupilotmega.MAV_MISSION_TYPE_MISSION,
 	})
 
 	return &pb.GotoWaypointResponse{CommandSucceeded: true}, nil
@@ -98,19 +100,19 @@ func (s *mastermindServiceServer) SetAttitude(stream pb.MastermindService_SetAtt
 
 	// set mode to FBWB ONE TIME ONLY
 
-	//s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
-	//	TargetSystem:    s.sysId,
-	//	TargetComponent: s.compId,
-	//	Command:         ardupilotmega.MAV_CMD_DO_SET_MODE,
-	//	Confirmation:    0,
-	//	Param1:          float32(ardupilotmega.PLANE_MODE_FLY_BY_WIRE_A), // PARAM1 MUST BE MAIN MODE (FBWA)
-	//	Param2:          float32(ardupilotmega.PLANE_MODE_FLY_BY_WIRE_B), // PARAM2 MUST BE SUBMODE (FBWB)
-	//	Param3:          0,
-	//	Param4:          0,
-	//	Param5:          0,
-	//	Param6:          0,
-	//	Param7:          0,
-	//})
+	s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
+		TargetSystem:    s.sysId,
+		TargetComponent: s.compId,
+		Command:         ardupilotmega.MAV_CMD_DO_SET_MODE,
+		Confirmation:    0,
+		Param1:          26, // PARAM1 MUST BE MAIN MODE (koustech)
+		Param2:          0, // PARAM2 MUST BE SUBMODE
+		Param3:          0,
+		Param4:          0,
+		Param5:          0,
+		Param6:          0,
+		Param7:          0,
+	})
 
 	// TODO: implement
 	// for every received message, send command and update target object (to be sent in detailed telem)
@@ -126,87 +128,20 @@ func (s *mastermindServiceServer) SetAttitude(stream pb.MastermindService_SetAtt
 		if err != nil {
 			break
 		}
-		//s.node.WriteMessageAll(&ardupilotmega.MessageSetAttitudeTarget{
-		//	TimeBootMs:      uint32(time.Now().UnixMilli()),
-		//	TargetSystem:    s.sysId,
-		//	TargetComponent: s.compId,
-		//	TypeMask: ardupilotmega.ATTITUDE_TARGET_TYPEMASK_THROTTLE_IGNORE |
-		//		ardupilotmega.ATTITUDE_TARGET_TYPEMASK_BODY_ROLL_RATE_IGNORE |
-		//		ardupilotmega.ATTITUDE_TARGET_TYPEMASK_BODY_PITCH_RATE_IGNORE |
-		//		ardupilotmega.ATTITUDE_TARGET_TYPEMASK_BODY_YAW_RATE_IGNORE,
-		//	Q:             [4]float32{float32(quat.W), float32(quat.X), float32(quat.Y), float32(quat.Z)},
-		//	BodyRollRate:  0,
-		//	BodyPitchRate: 0,
-		//	BodyYawRate:   0,
-		//	Thrust:        0,
-		//	ThrustBody:    [3]float32{},
-		//})
 
-		//utils.Logger.Info("roll = ch 1 = ", attitudeRequest.Roll)
-		//utils.Logger.Info("pitch = ch 2 = ", attitudeRequest.Pitch)
-		//s.node.WriteMessageAll(&ardupilotmega.MessageRcChannelsOverride{
-		//	TargetSystem:    s.sysId,
-		//	TargetComponent: s.compId,
-		//	Chan1Raw:        1100, //roll
-		//	Chan2Raw:        1500, //pitch
-		//	Chan3Raw:        math.MaxUint16,
-		//	Chan4Raw:        math.MaxUint16,
-		//	Chan5Raw:        math.MaxUint16,
-		//	Chan6Raw:        math.MaxUint16,
-		//	Chan7Raw:        math.MaxUint16,
-		//	Chan8Raw:        math.MaxUint16,
-		//	Chan9Raw:        0,
-		//	Chan10Raw:       0,
-		//	Chan11Raw:       0,
-		//	Chan12Raw:       0,
-		//	Chan13Raw:       0,
-		//	Chan14Raw:       0,
-		//	Chan15Raw:       0,
-		//	Chan16Raw:       0,
-		//	Chan17Raw:       0,
-		//	Chan18Raw:       0,
-		//})
-
-		//s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
-		//	TargetSystem:    s.sysId,
-		//	TargetComponent: s.compId,
-		//	Command:         ardupilotmega.MAV_CMD_DO_SET_SERVO,
-		//	Confirmation:    0,
-		//	Param1:          1,
-		//	Param2:          attitudeRequest.Roll,
-		//	Param3:          0,
-		//	Param4:          0,
-		//	Param5:          0,
-		//	Param6:          0,
-		//	Param7:          0,
-		//})
-		//s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
-		//	TargetSystem:    s.sysId,
-		//	TargetComponent: s.compId,
-		//	Command:         ardupilotmega.MAV_CMD_DO_SET_SERVO,
-		//	Confirmation:    0,
-		//	Param1:          2,
-		//	Param2:          attitudeRequest.Pitch,
-		//	Param3:          0,
-		//	Param4:          0,
-		//	Param5:          0,
-		//	Param6:          0,
-		//	Param7:          0,
-		//})
-
-		//s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
-		//	TargetSystem:    s.sysId,
-		//	TargetComponent: s.compId,
-		//	Command:         ardupilotmega.MAV_CMD_NAV_ATTITUDE_TIME,
-		//	Confirmation:    0,
-		//	Param1:          1,
-		//	Param2:          attitudeRequest.Roll,
-		//	Param3:          attitudeRequest.Pitch,
-		//	Param4:          0,
-		//	Param5:          3,
-		//	Param6:          0,
-		//	Param7:          0,
-		//})
+		s.node.WriteMessageAll(&ardupilotmega.MessageCommandLong{
+			TargetSystem:    s.sysId,
+			TargetComponent: s.compId,
+			Command:         MAV_CMD_NAV_LOCK_KSTCH,
+			Confirmation:    0,
+			Param1:          attitudeRequest.Roll * float32(attitudeRequest.Target.X),
+			Param2:          attitudeRequest.Pitch * float32(attitudeRequest.Target.Y),
+			Param3:          0,
+			Param4:          0,
+			Param5:          0,
+			Param6:          0,
+			Param7:          0,
+		})
 
 		s.target = attitudeRequest.Target
 	}
